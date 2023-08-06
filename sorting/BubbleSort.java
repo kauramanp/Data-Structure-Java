2fc5ab8b88971cb2817e72e7612de8aedacb475f
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-08-07 03:35:04 +0530

    Comments added

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -2,6 +2,22 @@ package sorting;
 
 //it takes the largest number to the last position 
 //then moves to next iteration by decreasing the size by 1 as the largest element is set
+/*
+ *  After sorting Phase 1
+        42      35      12      77      5       101
+    After sorting Phase 2
+        35      12      42      5       77      101
+    After sorting Phase 3
+        12      35      5       42      77      101
+    After sorting Phase 4
+        12      5       35      42      77      101
+    After sorting Phase 5
+        5       12      35      42      77      101
+    After sorting Phase 6
+        5       12      35      42      77      101
+    After sorting
+        5       12      35      42      77      101
+ */
 public class BubbleSort {
     public static void main(String args[]) {
         int[] a = { 77, 42, 35, 12, 101, 5 };
@@ -17,6 +33,11 @@ public class BubbleSort {
                     a[j] = temp;
                 }
             }
+            System.out.println("After sorting Phase " + (i + 1));
+            for (int k = 0; k < a.length; k++) {
+                System.out.print("\t" + a[k]);
+            }
+            System.out.println();
         }
 
         System.out.println("After sorting");
--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,37 +1,39 @@
 package sorting;
 
-import java.util.Scanner;
-
+/*
+ * After sorting Phase 2
+        11      12      13      5       6
+    After sorting Phase 3
+        11      12      13      5       6
+    After sorting Phase 4
+        5       11      12      13      6
+    After sorting Phase 5
+        5       6       11      12      13
+    After sorting
+        5       6       11      12      13
+ */
 public class InsertionSort {
     public static void main(String args[]) {
-        System.out.println("Enter the size of array");
-        Scanner scanner = new Scanner(System.in);
-        // int size = scanner.nextInt();
-        int size = 5;
-        // int[] array = new int[size];
-        // for (int i = 0; i < size; i++) {
-        // System.out.println("Enter the element to store at " + (i + 1) + " position");
-        // array[i] = scanner.nextInt();
-        // }
         int[] array = { 12, 11, 13, 5, 6 };
 
         for (int i = 1; i < array.length; i++) {
-            int temp = array[i];
+            int key = array[i];
             int j = i - 1;
-            for (; (j >= 0 && array[j] > temp); j--) {
-                array[j + i] = array[j];
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
             }
-            array[j + 1] = temp;
+            array[j + 1] = key;
+            System.out.println("After sorting Phase " + (i + 1));
+            for (int k = 0; k < array.length; k++) {
+                System.out.print("\t" + array[k]);
+            }
+            System.out.println();
         }
 
-        System.out.println("Sorted array");
-        for (int i = 1; i < len; i++) {
-            int key = arr[i];
-            int j = i - 1;
-            for (; (j >= 0 && arr[j] > key); j--) {
-                arr[j + 1] = arr[j];
-            }
-            arr[j + 1] = key;
+        System.out.println("After sorting");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("\t" + array[i]);
         }
     }
 }
