3e124b261b27e3e748cc01cfc46fd7c763f6ef86
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-08-09 23:53:00 +0530

    Added code

--- sorting/MergeSort.java
+++ sorting/MergeSort.java
@@ -4,17 +4,40 @@ public class MergeSort {
     public void divideArray(int[] array, int lowerBound, int upperBound) {
         if (lowerBound < upperBound) {
             int middleElement = (lowerBound + upperBound) / 2;
-            divideArray(array, lowerBound, middleElement - 1);
+            divideArray(array, lowerBound, middleElement);
             divideArray(array, middleElement + 1, upperBound);
             mergeArray(array, lowerBound, upperBound, middleElement);
         }
     }
 
     public void mergeArray(int[] array, int lowerBound, int upperBound, int middleElement) {
-        int inSorting;
-        if (array[lowerBound] > array[upperBound]) {
-            inSorting = array[lowerBound];
-            array[lowerBound] = inSorting;
+        int b[] = new int[array.length];
+        int i = lowerBound;
+        int j = middleElement + 1;
+        int k = lowerBound;
+        while (i <= middleElement && j <= upperBound) {
+            System.out.println("i " + i + " j " + j + " k " + k);
+            if (array[i] < array[j]) {
+                b[k] = array[i];
+                i++;
+                k++;
+            } else if (array[i] > array[j]) {
+                b[k] = array[j];
+                j++;
+                k++;
+            }
+        }
+
+        while (i < middleElement) {
+            array[k] = array[i];
+            i++;
+            k++;
+        }
+
+        while (j < upperBound) {
+            array[k] = array[j];
+            j++;
+            k++;
         }
     }
 
@@ -22,6 +45,8 @@ public class MergeSort {
         MergeSort mergeSort = new MergeSort();
         int[] array = { 12, 11, 13, 5, 6 };
         mergeSort.divideArray(array, 0, array.length);
-
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("\t" + array[i]);
+        }
     }
 }
