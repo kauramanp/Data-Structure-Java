e6f397b295804e12b132ad83931ae901759e477c
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-08-13 23:58:33 +0530

    quick sort completed

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -4,35 +4,44 @@ public class QuickSort {
     public int partition(int[] array, int lowerBound, int upperBound) {
         int i = lowerBound;
         int j = upperBound;
-        int pivotElement = array[lowerBound];
-        while (i < j) {
-            do {
+        int pivotElement = array[(lowerBound + upperBound) / 2];
+        System.out.println("Pivot " + pivotElement + " lowerbound " + lowerBound + "higher bound " + upperBound);
+        while (i <= j) {
+            while (array[i] < pivotElement) {
                 i++;
-            } while (array[i] <= pivotElement);
-            do {
+            }
+            ;
+            while (array[j] > pivotElement) {
                 j--;
-            } while (array[j] > pivotElement);
-            if (i < j) {
+            }
+            ;
+            System.out.println("i " + i + " array[i] " + array[i] + " j " + j + " array[j] " + array[j]);
+            if (i <= j) {
                 int temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
+                i++;
+                j--;
+                display(array, lowerBound, upperBound);
             }
 
         }
-        int temp = array[lowerBound];
-        array[lowerBound] = array[j];
-        array[j] = temp;
-        return j;
+
+        return i;
 
     }
 
     public void quickSort(int array[], int lowerBound, int upperBound) {
 
-        if (lowerBound < upperBound) {
-            display(array, lowerBound, upperBound);
-            int p = partition(array, lowerBound, upperBound);
-            quickSort(array, lowerBound, p);
-            quickSort(array, p + 1, upperBound);
+        display(array, lowerBound, upperBound);
+        int p = partition(array, lowerBound, upperBound);
+        System.out.println("Returned value " + p);
+        if (lowerBound < (p - 1)) {
+            quickSort(array, lowerBound, p - 1);
+        }
+        if (p < upperBound) {
+            quickSort(array, p, upperBound);
+
         }
     }
 
@@ -49,6 +58,7 @@ public class QuickSort {
         int[] array = { 12, 11, 13, 5, 6, 2 };
         // int[] array = { 12, 31, 25, 8, 32, 17, 40, 42 };
         // int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        // int[] array = { 15, 9, 7, 13, 12, 16, 4, 18, 11 };
         quickSort.quickSort(array, 0, array.length - 1);
         quickSort.display(array, 0, array.length - 1);
     }
